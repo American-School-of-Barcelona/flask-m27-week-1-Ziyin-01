a9383623dd1e1f35969c128d27aedbab84ede538
Ziyin-01 <ziyin@asbarcelona.com>
2026-02-23 08:26:18 +0000

    complete

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask
+from flask import Flask , render_template, request
 
 app = Flask(__name__)
 
 # TODO: Add your routes below this line
 
+tasks = [
+    {"title" : "Complete math practice"},
+    {"title" : "Complete Paper 2 draft for English"},
+    {"title" : "Complete financial report before Wednesday"},
+]
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/")
+def about():
+    return render_template("about.html")
+
+@app.route("/task")
+def task():
+    return render_template("task.html", tasks=tasks)
+
+@app.route("/here")
+def here():
+    return render_template("here.html")
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+#@app.route("/", methods=["GET", "POST"])
+#def index():
+    #if request.method == "POST":
+        #return render_template("about.html", name=request.form.get("name", "world"))
+    #else:
+        #return render_template("index.html")
+
+#if __name__ == "__main__":
+    #app.run(debug=True)
